38ffcbe59a8ec4aaa9ffe7f51d7c23e6c15450ea
Jason Nguyen <jnguyen7473@gmail.com>
2025-03-05 04:47:52 -0800

    added names

--- buff_objects.py
+++ buff_objects.py
@@ -23,54 +23,55 @@ Character Buffs
 """
 
 # TODO: MAKE DICTIONARIES
-AstraYao = CharacterBuff("Astra Yao", 0, 1200, 0, 25, 20, 0, 0, 0,
+AstraYao = CharacterBuff("AstraYao", 0, 1200, 0, 25, 20, 0, 0, 0,
                          0, "", [ATTACKER, ANOMALY], SUPPORTER, STARS_OF_LYRA, ETHER)
 
-HarumasaSelfBuff = Buff("Harumasa Self Buff", 0, 0, 25, 72, 0, 0, 0, 0)
+HarumasaSelfBuff = Buff("HarumasaSelfBuff", 0, 0, 25, 72, 0, 0, 0, 0)
 
-NicoleM6 = CharacterBuff("Nicole", 0, 0, 15, 0, 0, 0, 40, 0,
+NicoleM6 = CharacterBuff("NicoleM6", 0, 0, 15, 0, 0, 0, 40, 0,
                          25, TYPE_BONUS_DMG, [ETHER, CUNNING_HARES], SUPPORTER, CUNNING_HARES, ETHER)
 
-RinaM0SliceOfTime = CharacterBuff("Rina", 0, 0, 0, 0, 0, 28.6, 0, 0,
+RinaM0SliceOfTime = CharacterBuff("RinaM0SliceOfTime", 0, 0, 0, 0, 0, 28.6, 0, 0,
                                   10, TYPE_BONUS_DMG, [ELECTRIC, VICTORIA_HOUSEKEEPING], SUPPORTER, VICTORIA_HOUSEKEEPING, ELECTRIC)
 
 """
 Set Buffs
 """
-AstralVoice = Buff("", 0, 0, 0, 0, 24, 0, 0, 0)
-SwingJazz = Buff("", 0, 0, 0, 0, 15, 0, 0, 0)
-WoodpeckerElectro1 = Buff("", 9, 0, 0, 0, 0, 0, 0, 0)
-WoodpeckerElectro2 = Buff("", 18, 0, 0, 0, 0, 0, 0, 0)
-WoodpeckerElectro3 = Buff("", 27, 0, 0, 0, 0, 0, 0, 0)
+AstralVoice = Buff("AstralVoice", 0, 0, 0, 0, 24, 0, 0, 0)
+SwingJazz = Buff("SwingJazz", 0, 0, 0, 0, 15, 0, 0, 0)
+WoodpeckerElectro1 = Buff("WoodpeckerElectro1", 9, 0, 0, 0, 0, 0, 0, 0)
+WoodpeckerElectro2 = Buff("WoodpeckerElectro2", 18, 0, 0, 0, 0, 0, 0, 0)
+WoodpeckerElectro3 = Buff("WoodpeckerElectro3", 27, 0, 0, 0, 0, 0, 0, 0)
 
 
 """
 Weapon Buffs
 DOES NOT INCLUDE UNCONDITIONAL STATS
 """
-MarcatoDesire0 = Weapon("", 594, 0, 0, 0, 0, 0, 0, 0, 0)
-MarcatoDesire1 = Weapon("", 594, 9.6, 0, 0, 0, 0, 0, 0, 0)
-MarcatoDesire2 = Weapon("", 594, 19.2, 0, 0, 0, 0, 0, 0, 0)
+MarcatoDesire0 = Weapon("MarcatoDesire0", 594, 0, 0, 0, 0, 0, 0, 0, 0)
+MarcatoDesire1 = Weapon("MarcatoDesire1", 594, 9.6, 0, 0, 0, 0, 0, 0, 0)
+MarcatoDesire2 = Weapon("MarcatoDesire2", 594, 19.2, 0, 0, 0, 0, 0, 0, 0)
 
-StarlightEngine0 = MarcatoDesire0
-StarlightEngine1 = MarcatoDesire2
+StarlightEngine0 = Weapon("StarlightEngine0", 594, 0, 0, 0, 0, 0, 0, 0, 0)
+StarlightEngine1 = Weapon("StarlightEngine1", 594, 19.2, 0, 0, 0, 0, 0, 0, 0)
 
-TheBrimstone0 = Weapon("", 684, 0, 0, 0, 0, 0, 0, 0, 0)
-TheBrimstone1 = Weapon("", 684, 3.5, 0, 0, 0, 0, 0, 0, 0)
-TheBrimstone2 = Weapon("", 684, 7, 0, 0, 0, 0, 0, 0, 0)
-TheBrimstone3 = Weapon("", 684, 10.5, 0, 0, 0, 0, 0, 0, 0)
-TheBrimstone4 = Weapon("", 684, 14, 0, 0, 0, 0, 0, 0, 0)
-TheBrimstone5 = Weapon("", 684, 17.5, 0, 0, 0, 0, 0, 0, 0)
-TheBrimstone6 = Weapon("", 684, 21, 0, 0, 0, 0, 0, 0, 0)
-TheBrimstone7 = Weapon("", 684, 24.5, 0, 0, 0, 0, 0, 0, 0)
-TheBrimstone8 = Weapon("", 684, 28, 0, 0, 0, 0, 0, 0, 0)
+
+TheBrimstone0 = Weapon("TheBrimstone0", 684, 0, 0, 0, 0, 0, 0, 0, 0)
+TheBrimstone1 = Weapon("TheBrimstone1", 684, 3.5, 0, 0, 0, 0, 0, 0, 0)
+TheBrimstone2 = Weapon("TheBrimstone2", 684, 7, 0, 0, 0, 0, 0, 0, 0)
+TheBrimstone3 = Weapon("TheBrimstone3", 684, 10.5, 0, 0, 0, 0, 0, 0, 0)
+TheBrimstone4 = Weapon("TheBrimstone4", 684, 14, 0, 0, 0, 0, 0, 0, 0)
+TheBrimstone5 = Weapon("TheBrimstone5", 684, 17.5, 0, 0, 0, 0, 0, 0, 0)
+TheBrimstone6 = Weapon("TheBrimstone6", 684, 21, 0, 0, 0, 0, 0, 0, 0)
+TheBrimstone7 = Weapon("TheBrimstone7", 684, 24.5, 0, 0, 0, 0, 0, 0, 0)
+TheBrimstone8 = Weapon("TheBrimstone8", 684, 28, 0, 0, 0, 0, 0, 0, 0)
 
 
 """
 APPLIED BUFFS NEEDS TO HAVE WEAPON AT INDEX 0
 """
 SelectedWeapon = StarlightEngine1
-CustomBuff = CharacterBuff("", 0, 0, 0, 0, 0,
+CustomBuff = CharacterBuff("Custom", 0, 0, 0, 0, 0,
                            0, 0, 0,
                            0, "", "",
                            STUNNER, "", ELECTRIC)
@@ -93,4 +94,5 @@ team3 = [SelectedWeapon, HarumasaSelfBuff, NicoleM6, SwingJazz, AstralVoice, Woo
 
 harumasa_teams = [team1, team2, team3]
 
-print(Buff._registry)
+for buff in Buff._registry:
+    print(buff.name)
