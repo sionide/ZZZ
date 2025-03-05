64885fe95ea1dcfe59920c2c432777a6a8c174ec
Jason Nguyen <jnguyen7473@gmail.com>
2025-03-05 04:37:53 -0800

    Added name to Buff and subclasses

--- buff_objects.py
+++ buff_objects.py
@@ -22,55 +22,55 @@ TYPE_CRIT_DMG = "crit dmg"
 Character Buffs
 """
 
-# TODO: ADD CORE BONUS AND FACTION
-AstraYao = CharacterBuff(0, 1200, 0, 25, 20, 0, 0, 0,
+# TODO: MAKE DICTIONARIES
+AstraYao = CharacterBuff("Astra Yao", 0, 1200, 0, 25, 20, 0, 0, 0,
                          0, "", [ATTACKER, ANOMALY], SUPPORTER, STARS_OF_LYRA, ETHER)
 
-HarumasaSelfBuff = Buff(0, 0, 25, 72, 0, 0, 0, 0)
+HarumasaSelfBuff = Buff("Harumasa Self Buff", 0, 0, 25, 72, 0, 0, 0, 0)
 
-NicoleM6 = CharacterBuff(0, 0, 15, 0, 0, 0, 40, 0,
+NicoleM6 = CharacterBuff("Nicole", 0, 0, 15, 0, 0, 0, 40, 0,
                          25, TYPE_BONUS_DMG, [ETHER, CUNNING_HARES], SUPPORTER, CUNNING_HARES, ETHER)
 
-RinaM0SliceOfTime = CharacterBuff(0, 0, 0, 0, 0, 28.6, 0, 0,
+RinaM0SliceOfTime = CharacterBuff("Rina", 0, 0, 0, 0, 0, 28.6, 0, 0,
                                   10, TYPE_BONUS_DMG, [ELECTRIC, VICTORIA_HOUSEKEEPING], SUPPORTER, VICTORIA_HOUSEKEEPING, ELECTRIC)
 
 """
 Set Buffs
 """
-AstralVoice = Buff(0, 0, 0, 0, 24, 0, 0, 0)
-SwingJazz = Buff(0, 0, 0, 0, 15, 0, 0, 0)
-WoodpeckerElectro1 = Buff(9, 0, 0, 0, 0, 0, 0, 0)
-WoodpeckerElectro2 = Buff(18, 0, 0, 0, 0, 0, 0, 0)
-WoodpeckerElectro3 = Buff(27, 0, 0, 0, 0, 0, 0, 0)
+AstralVoice = Buff("", 0, 0, 0, 0, 24, 0, 0, 0)
+SwingJazz = Buff("", 0, 0, 0, 0, 15, 0, 0, 0)
+WoodpeckerElectro1 = Buff("", 9, 0, 0, 0, 0, 0, 0, 0)
+WoodpeckerElectro2 = Buff("", 18, 0, 0, 0, 0, 0, 0, 0)
+WoodpeckerElectro3 = Buff("", 27, 0, 0, 0, 0, 0, 0, 0)
 
 
 """
 Weapon Buffs
 DOES NOT INCLUDE UNCONDITIONAL STATS
 """
-MarcatoDesire0 = Weapon(594, 0, 0, 0, 0, 0, 0, 0, 0)
-MarcatoDesire1 = Weapon(594, 9.6, 0, 0, 0, 0, 0, 0, 0)
-MarcatoDesire2 = Weapon(594, 19.2, 0, 0, 0, 0, 0, 0, 0)
+MarcatoDesire0 = Weapon("", 594, 0, 0, 0, 0, 0, 0, 0, 0)
+MarcatoDesire1 = Weapon("", 594, 9.6, 0, 0, 0, 0, 0, 0, 0)
+MarcatoDesire2 = Weapon("", 594, 19.2, 0, 0, 0, 0, 0, 0, 0)
 
 StarlightEngine0 = MarcatoDesire0
 StarlightEngine1 = MarcatoDesire2
 
-TheBrimstone0 = Weapon(684, 0, 0, 0, 0, 0, 0, 0, 0)
-TheBrimstone1 = Weapon(684, 3.5, 0, 0, 0, 0, 0, 0, 0)
-TheBrimstone2 = Weapon(684, 7, 0, 0, 0, 0, 0, 0, 0)
-TheBrimstone3 = Weapon(684, 10.5, 0, 0, 0, 0, 0, 0, 0)
-TheBrimstone4 = Weapon(684, 14, 0, 0, 0, 0, 0, 0, 0)
-TheBrimstone5 = Weapon(684, 17.5, 0, 0, 0, 0, 0, 0, 0)
-TheBrimstone6 = Weapon(684, 21, 0, 0, 0, 0, 0, 0, 0)
-TheBrimstone7 = Weapon(684, 24.5, 0, 0, 0, 0, 0, 0, 0)
-TheBrimstone8 = Weapon(684, 28, 0, 0, 0, 0, 0, 0, 0)
+TheBrimstone0 = Weapon("", 684, 0, 0, 0, 0, 0, 0, 0, 0)
+TheBrimstone1 = Weapon("", 684, 3.5, 0, 0, 0, 0, 0, 0, 0)
+TheBrimstone2 = Weapon("", 684, 7, 0, 0, 0, 0, 0, 0, 0)
+TheBrimstone3 = Weapon("", 684, 10.5, 0, 0, 0, 0, 0, 0, 0)
+TheBrimstone4 = Weapon("", 684, 14, 0, 0, 0, 0, 0, 0, 0)
+TheBrimstone5 = Weapon("", 684, 17.5, 0, 0, 0, 0, 0, 0, 0)
+TheBrimstone6 = Weapon("", 684, 21, 0, 0, 0, 0, 0, 0, 0)
+TheBrimstone7 = Weapon("", 684, 24.5, 0, 0, 0, 0, 0, 0, 0)
+TheBrimstone8 = Weapon("", 684, 28, 0, 0, 0, 0, 0, 0, 0)
 
 
 """
 APPLIED BUFFS NEEDS TO HAVE WEAPON AT INDEX 0
 """
 SelectedWeapon = StarlightEngine1
-CustomBuff = CharacterBuff(0, 0, 0, 0, 0,
+CustomBuff = CharacterBuff("", 0, 0, 0, 0, 0,
                            0, 0, 0,
                            0, "", "",
                            STUNNER, "", ELECTRIC)
@@ -81,10 +81,10 @@ applied_buffs = [SelectedWeapon,HarumasaSelfBuff,
                  AstralVoice,
                  SwingJazz,
                  WoodpeckerElectro2,
-                 # NicoleM6,
-                 # RinaM0SliceOfTime,
-                 CustomBuff,
-                 CustomBuff
+                 NicoleM6,
+                 RinaM0SliceOfTime,
+                 # CustomBuff,
+                 # CustomBuff
                  ]
 
 team1 = [SelectedWeapon, HarumasaSelfBuff, AstraYao, NicoleM6, SwingJazz, AstralVoice, WoodpeckerElectro2]
@@ -92,3 +92,5 @@ team2 = [SelectedWeapon, HarumasaSelfBuff, AstraYao, SwingJazz, AstralVoice, Woo
 team3 = [SelectedWeapon, HarumasaSelfBuff, NicoleM6, SwingJazz, AstralVoice, WoodpeckerElectro2]
 
 harumasa_teams = [team1, team2, team3]
+
+print(Buff._registry)
--- buffs.py
+++ buffs.py
@@ -14,14 +14,23 @@ class CharacterDescription:
         OVERLOADED == OPERATOR TO CHECK FOR A MATCHING DESCRIPTION
         """
         for descriptor in self.descriptions:
-            print("{0} == {1}".format(other, descriptor))
+            # print("{0} == {1}".format(other, descriptor))
             if other == descriptor:
                 return True
-            print("NO MATCH")
+            # print("NO MATCH")
         return False
 
+class IterRegistry(type):
+    def __iter__(cls):
+        return iter(cls._registry)
+
 class Buff:
-    def __init__(buff, atk_percent, flat_atk, crit_rate, crit_dmg, bonus_dmg, pen_ratio, def_reduction, res_ignore):
+    __metaclass__ = IterRegistry
+    _registry = []
+
+    def __init__(buff, name, atk_percent, flat_atk, crit_rate, crit_dmg, bonus_dmg, pen_ratio, def_reduction, res_ignore):
+        buff._registry.append(buff)
+
         buff.atk_percent = atk_percent
         buff.flat_atk = flat_atk
         buff.crit_rate = crit_rate
@@ -33,16 +42,16 @@ class Buff:
 
 # TODO: ADD CORE BONUS CONDITIONS
 class CharacterBuff(Buff):
-    def __init__(self, atk_percent, flat_atk, crit_rate, crit_dmg, bonus_dmg, pen_ratio, def_reduction, res_ignore,
+    def __init__(self, name, atk_percent, flat_atk, crit_rate, crit_dmg, bonus_dmg, pen_ratio, def_reduction, res_ignore,
                  core_bonus, core_bonus_type, core_bonus_conditions, specialty, faction, attribute):
-        super().__init__(atk_percent, flat_atk, crit_rate, crit_dmg, bonus_dmg, pen_ratio, def_reduction, res_ignore)
+        super().__init__(name, atk_percent, flat_atk, crit_rate, crit_dmg, bonus_dmg, pen_ratio, def_reduction, res_ignore)
         self.core_bonus = core_bonus
         self.core_bonus_type = core_bonus_type
         self.core_bonus_conditions = core_bonus_conditions
         self.MyDescription = CharacterDescription(specialty, faction, attribute)
 
 class Weapon(Buff):
-    def __init__(self, weapon_base, atk_percent, flat_atk, crit_rate, crit_dmg, bonus_dmg, pen_ratio, def_reduction,
+    def __init__(self, name, weapon_base, atk_percent, flat_atk, crit_rate, crit_dmg, bonus_dmg, pen_ratio, def_reduction,
                  res_ignore):
-        super().__init__(atk_percent, flat_atk, crit_rate, crit_dmg, bonus_dmg, pen_ratio, def_reduction, res_ignore)
+        super().__init__(name, atk_percent, flat_atk, crit_rate, crit_dmg, bonus_dmg, pen_ratio, def_reduction, res_ignore)
         self.weapon_base = weapon_base
